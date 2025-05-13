4ff51a1bafc46c064d0b540707914968cde5307f
Nuhamin Emnebered <166820068+nuhajoy@users.noreply.github.com>
2025-05-13 10:41:04 +0300

    SalesRepositoryImpl.kt

--- app/src/main/java/com/example/shopapp/infrasturacture/repositories/SalesRepositoryImpl.kt
+++ app/src/main/java/com/example/shopapp/infrasturacture/repositories/SalesRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.example.coffeeshopapp.data.repository
 import com.example.shopapp.infrastructure.datasources.SalesApi
 import com.example.shopapp.domain.entities.SalesData
 
-// Make sure you have this interface defined somewhere:
+
 // interface SalesRepository { suspend fun getSalesData(): SalesData }
 
 class SalesRepositoryImpl : SalesRepository {
